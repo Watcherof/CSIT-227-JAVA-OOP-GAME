f30d402b20fe957b1759245b5a1c9f8f13933fcf
jones21312321313213 <skinnyjones212121@gmail.com>
2024-10-14 08:39:27 +0800

    aa
    a

--- Lores.java
+++
@@ -1,8 +0,0 @@
-public interface Lores{
-    public void tellLoreGuardian();
-    public void tellLoreGeneral();
-    public void tellLoreEmber();
-    public void tellLoreTempest();
-    public void tellLoreArcane();
-    public void tellLoreShadow();
-}
