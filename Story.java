a9f73b068a37d2e8078fe2392ab22563c045e7f5
jones21312321313213 <skinnyjones212121@gmail.com>
2024-10-16 14:26:10 +0800

    Update Story.java

--- Story.java
+++ Story.java
@@ -1,4 +1,4 @@
-public class Story implements Stories {
+public class Story implements StoryInterface {
     // this lets you input a string of text and splits text into words and adds delay in MilliSeconds
     @Override
     public void displayWithDelay(String text, int delayInMillis) {
